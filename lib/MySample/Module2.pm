558cffcd833f75b3233ded98ce678129a4dda3a2
yowcow <yowcow@cpan.org>
2016-08-15 07:49:48 +0000

    implement

--- lib/MySample/Module2.pm
+++ lib/MySample/Module2.pm
@@ -2,10 +2,34 @@ package MySample::Module2;
 use strict;
 use warnings;
 
-our $VERSION = 'v0.0.1';
+our $VERSION = '0.0.1';
 
-sub foo {
-    'bar';
+use File::Basename qw(dirname);
+use File::Spec;
+use Moo;
+use strictures 2;
+use namespace::clean;
+
+has fh => (
+    is      => 'ro',
+    default => sub {
+        my $path = File::Spec->catfile(dirname(__FILE__), 'Module2', 'data.txt');
+        open my $fh, '<', $path or die "Failed opening file: $!";
+        binmode $fh, ':utf8';
+        $fh;
+    },
+);
+
+sub next {
+    my $self = shift;
+    my $fh   = $self->fh;
+    my $ret  = <$fh>;
+    $ret
+        ? do {
+        $ret =~ s/\R//sg;
+        $ret;
+        }
+        : undef;
 }
 
 1;
--- t/01-module2.t
+++ t/01-module2.t
@@ -1,8 +1,14 @@
 use strict;
+use utf8;
 use warnings;
 use MySample::Module2;
 use Test::More;
 
-is(MySample::Module2->foo, 'bar');
+my $mod = MySample::Module2->new;
+
+is $mod->next, 'あ';
+is $mod->next, 'い';
+is $mod->next, 'う';
+is $mod->next, undef;
 
 done_testing;
